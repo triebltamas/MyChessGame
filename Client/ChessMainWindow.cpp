0e097e7ea7c66018665a010995fd725f8ed4cbd0
Triebl Tamás <triebltamas2000@gmail.com>
2022-04-04 21:05:25 +0200

    implementing login widget

--- Client/ChessMainWindow.cpp
+++ Client/ChessMainWindow.cpp
@@ -10,17 +10,63 @@ ChessMainWindow::ChessMainWindow(QWidget *parent)
   connect(ui->actionExit, &QAction::triggered, this, &ChessMainWindow::exit);
   connect(ui->actionHomePage, &QAction::triggered, this,
           &ChessMainWindow::homePage);
+
+  ui->menubar->setVisible(false);
+  // LOGIN WIDGET
+  loginWidget_ = new LoginWidget();
+
+  connect(loginWidget_, &LoginWidget::loggedIn, this,
+          &ChessMainWindow::onLoggedIn);
+
+  connect(loginWidget_, &LoginWidget::networkSettingsChanged, this,
+          &ChessMainWindow::onNetworkSettingsChanged);
+
+  ui->centralwidget->layout()->addWidget(loginWidget_);
+
+  // CONNECTING TO SERVER
+}
+
+ChessMainWindow::~ChessMainWindow() { delete ui; }
+
+void ChessMainWindow::onLoggedIn() {
+  // todo leellenorizni h jo e a bejelentkezes
+  // ha nem akkor loginWidget_->inCorrectSignIn();
+
+  ui->centralwidget->layout()->removeWidget(loginWidget_);
+
+  if (loginWidget_ != nullptr)
+    delete loginWidget_;
+
   homePageWidget_ = new HomePageWidget(this);
   ui->centralwidget->layout()->addWidget(homePageWidget_);
   connect(homePageWidget_, &HomePageWidget::localGameButtonClicked, this,
           &ChessMainWindow::onLocalGameClicked);
   connect(homePageWidget_, &HomePageWidget::onlineGameButtonClicked, this,
           &ChessMainWindow::onOnlineGameClicked);
+
+  ui->menubar->setVisible(true);
+}
+void ChessMainWindow::onNetworkSettingsChanged(QString serverAddress,
+                                               int requestPort,
+                                               int responsePort) {
+  if (loginWidget_ == nullptr)
+    return;
+
+  serverAddress_ = serverAddress;
+  requestPort_ = requestPort;
+  responsePort_ = responsePort;
+
+  // todo reconnect to server
+  //  loginWidget_->setConnected(success);
+  loginWidget_->setConnected(true);
 }
 
-ChessMainWindow::~ChessMainWindow() { delete ui; }
-
 void ChessMainWindow::onConnectedToServer() {
+  if (loginWidget_ == nullptr)
+    return;
+
+  loginWidget_->setConnected(true);
+
   // todo login screen
 }
 void ChessMainWindow::onOnlineGameClicked() {
--- Client/ChessMainWindow.h
+++ Client/ChessMainWindow.h
@@ -5,6 +5,7 @@
 #include "ChessField.h"
 #include "HomePageWidget.h"
 #include "LocalChessWidget.h"
+#include "LoginWidget.h"
 #include "OnlineChessWidget.h"
 #include "SwitchPawnDialog.h"
 #include <QLabel>
@@ -28,6 +29,9 @@ public slots:
   void onConnectedToServer();
   void onOnlineGameClicked();
   void onLocalGameClicked();
+  void onLoggedIn();
+  void onNetworkSettingsChanged(QString serverAddress, int requestPort,
+                                int responsePort);
 
 private:
   void exit();
@@ -37,5 +41,10 @@ private:
   OnlineChessWidget *onlineWidget_ = nullptr;
   LocalChessWidget *localWidget_ = nullptr;
   HomePageWidget *homePageWidget_ = nullptr;
+  LoginWidget *loginWidget_ = nullptr;
+
+  QString serverAddress_ = "127.0.0.1";
+  int requestPort_ = 1337;
+  int responsePort_ = 1338;
 };
 #endif // CHESSMAINWINDOW_H
